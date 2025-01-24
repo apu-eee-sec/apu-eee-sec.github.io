46efcac860c1c6a4f706a5227df23dffd9508d58
Mahbub Hasan Apu <123699809+apu-eee-sec@users.noreply.github.com>
2025-01-24 21:32:44 +0600

    Update link.js

--- files/link.js
+++ files/link.js
@@ -2,7 +2,7 @@
         const HomeHref = "https://apu-eee-sec.netlify.app/";
 
         // Select all a tags with the 'shared-link' class
-        const HomeLinks = document.querySelectorAll('.Home-link');
+        const HomeLinks = document.querySelectorAll('.logo');
 
         // Loop through the selected elements and set the href
         HomeLinks.forEach(link => {
