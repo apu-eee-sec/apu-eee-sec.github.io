9a1df9f95bdbdc58337be2b2bf7e95af30b0410e
Mahbub Hasan Apu <123699809+apu-eee-sec@users.noreply.github.com>
2025-01-24 21:26:50 +0600

    Add files via upload

---
+++ files/link.js
@@ -0,0 +1,10 @@
+        // Define the shared href link
+        const HomeHref = "https://apu-eee-sec.netlify.app/";
+
+        // Select all a tags with the 'shared-link' class
+        const HomeLinks = document.querySelectorAll('.Home-link');
+
+        // Loop through the selected elements and set the href
+        HomeLinks.forEach(link => {
+            link.href = HomeHref;
+        });
